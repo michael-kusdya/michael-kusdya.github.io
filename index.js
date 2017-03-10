5bb30a8bb215a4a83b1d920d258eb535b8897248
Michael Kusdya <michael.kusdya@gmail.com>
2017-03-10 11:08:48 +0700

    minor fix

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-$(document).ready(function(){
-  $('.navbar .nav li a').on('click', function(event){     
-    event.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-  });  
-  
-    $("body").scrollspy({
-        target: ".navbar",
-        offset: 70
-    }) 
+$(document).ready(function () {
+	$('.navbar .nav li a').on('click', function (event) {
+		event.preventDefault();
+		$('html,body').animate({
+			scrollTop: $(this.hash).offset().top
+		}, 500);
+		$('.navbar-collapse').collapse('hide');
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 60
+		}, 300);
+	});
+
+	$("body").scrollspy({
+		target: ".navbar",
+		offset: 70
+	})
 
 });
